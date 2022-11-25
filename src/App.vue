08695a366b744d3a643d564d942b189b45e53ed1
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2022-11-25 10:21:55 +0700

    update deploy

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <VueElementHighlight :is-active="step === 1" label="This is an example of using vue-element-highlight for menu bar" @nextButtonClick="step= 1+1">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </VueElementHighlight>
+          </li>
+          <li class="nav-item">
+            <VueElementHighlight :is-active="step === 2" :is-close-label="true" 
+              label="You can view the Link menu here" 
+              @nextButtonClick="step= 2+1" @closeButtonClick="step = 0">
+              <a class="nav-link" href="#">Link</a>
+            </VueElementHighlight>
+          </li>
+          <li class="nav-item dropdown">
+            <VueElementHighlight :is-active="step === 3" label="You can view the Dropdown menu here" @nextButtonClick="step= 3+1">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </VueElementHighlight>
+          </li>
+          <li class="nav-item">
+            <VueElementHighlight :is-active="step === 4" :isExitOnOutside="true"
+              label="You can click outside to close this popup" 
+              @nextButtonClick="step= 4+1" @closeButtonClick="step = 0">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </VueElementHighlight>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <VueElementHighlight :is-active="step === 5" label="You can search everything here" @nextButtonClick="step= 0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </VueElementHighlight>
+        </form>
+      </div>
+    </nav>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VueElementHighlight from 'vue-element-highlight';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    VueElementHighlight
+  },
+  data() {
+    return {
+      step: 1
+    }
+  },
 }
 </script>
 
@@ -23,6 +74,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/HighlightTag.vue
@@ -0,0 +1,137 @@
+<template>
+    <div>
+      <div class="hgl-bg-active" v-if="isActive" @click="isExitOnOutside ? $emit('closeButtonClick', true) : null"></div>
+      <div class="hgl-wrapper" :class="{active: isActive}">
+        <div class="out-text">
+          <slot></slot>
+        </div>
+        <div class="hgl-box" :class="[position, boxClassCustom]" v-if="isActive">
+          <div class="hgl-arrow" :class="[position]"></div>
+          <div>{{label}}</div>
+          <div class="hgl-row-btn">
+            <div>
+              <a href="javascript:void(0)" class="hgl-close-btn" @click="$emit('closeButtonClick', true)"
+                 v-if="isCloseLabel">
+                {{closeLabel}}
+              </a>
+            </div>
+            <div>
+              <a href="javascript:void(0)" class="hgl-next-btn" @click="$emit('nextButtonClick', true)">
+                {{nextLabel}}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: "HighlightTag",
+    props: {
+      isActive: {
+        type: Boolean,
+        default: false
+      },
+      isExitOnOutside: {
+        type: Boolean,
+        default: true
+      },
+      isCloseLabel: {
+        type: Boolean,
+        default: false
+      },
+      label: {
+        type: String,
+        default: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+      },
+      nextLabel: {
+        type: String,
+        default: "Next"
+      },
+      closeLabel: {
+        type: String,
+        default: "Close"
+      },
+      boxClassCustom: {
+        type: String,
+        default: ''
+      },
+      position: {
+        type: String,
+        default: 'left'
+      }
+    }
+  
+  }
+  </script>
+  <style>
+  .hgl-bg-active {
+    box-sizing: content-box;
+    z-index: 999999;
+    background-color: #000;
+    inset: 0;
+    position: fixed;
+    opacity: 0.8;
+  }
+  .hgl-wrapper {
+    position: relative;
+  }
+  .hgl-wrapper.active {
+    z-index: 10000000;
+  }
+  .hgl-wrapper.active .out-text > * {
+    color: #000!important;
+  }
+  .hgl-box {
+    position: absolute;
+    left: 5px;
+    top: 45px;
+    padding: 10px;
+    min-width: 225px;
+    max-width: 275px;
+    background-color: #1B499F;
+    color: white;
+    text-align: left;
+    border-radius: 5px;
+    box-shadow: 0 1px 10px rgb(0 0 0 / 40%);
+    transition: opacity 0.1s ease-out;
+  }
+  .hgl-box.right {
+    left: unset;
+    right: 5px;
+  }
+  .hgl-box.top {
+    top: unset;
+    bottom: 45px;
+  }
+  .hgl-arrow {
+    border: 9px solid #1b499f;
+    content: "";
+    position: absolute;
+    top: -18px;
+    border-color: transparent transparent #1b499f;
+  }
+  .hgl-arrow.right {
+    right: 10px;
+  }
+  .hgl-arrow.top {
+    top: unset;
+    bottom: -18px;
+    border-color: #1b499f transparent transparent;
+  }
+  .hgl-row-btn {
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .hgl-close-btn {
+    color: #F2C94C;
+    text-decoration: none;
+  }
+  .hgl-next-btn {
+    color: #F2C94C;
+    text-decoration: none;
+  }
+  </style>
