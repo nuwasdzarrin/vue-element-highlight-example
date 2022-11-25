5068052e5994de5fedfdfd0bd193cfaf78d06f08
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2022-11-25 14:56:18 +0700

    update deploy stagged

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,6 @@ git add -A
 git commit -m 'deploy'
 git push -f git@github.com:nuwasdzarrin/vue-element-highlight-example.git master:gh-pages
 cd -
+# Lastly, make sure your script is executable!
+# chmod +x deploy.sh
+# source: https://medium.com/swlh/deploy-vue-app-to-github-pages-2ada48d7397e
